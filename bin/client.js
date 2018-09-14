5bdf5b0a3c997f867f18c0f8d5249c2b7259f79e
Dylan Van Assche <dylan.van.assche@protonmail.com>
2018-09-14 09:11:07 +0200

    Use local git CSA

--- bin/client.js
+++ bin/client.js
@@ -79,7 +79,7 @@ client.timespanQuery(q, function (stream, source, connectionsStream) {
     paths.push(path);
     console.log('-------------------------------------------')
     //console.log(path);
-    /*path.forEach(function (connection) {
+    path.forEach(function (connection) {
       console.log(connection.departureTime.toISOString() + " at " + connection.departureStop + " To arrive in " + connection.arrivalStop + " at " + connection.arrivalTime.toISOString());
       if (connection["gtfs:trip"]) {
         console.log(" with trip id " + JSON.stringify(connection["gtfs:trip"]));
@@ -96,7 +96,7 @@ client.timespanQuery(q, function (stream, source, connectionsStream) {
       sumResponseTimes += httpResponseTimes[url];
     }
     console.log("Downloading data over HTTP adds up to", sumResponseTimes, "ms");
-    console.log(Math.round(totalBytesTransfered / (1024 * 1024) * 100) / 100 + "MB transfered while answering this query");*/
+    console.log(Math.round(totalBytesTransfered / (1024 * 1024) * 100) / 100 + "MB transfered while answering this query");
   });
   /*stream.on('end', () => {
     console.log('**********************RESULTS: \n');
